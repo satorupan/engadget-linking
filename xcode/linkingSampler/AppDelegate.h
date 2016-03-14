2e28461f14023c982eec8631667e8fee43fa34eb
satorupan <satorupan@icloud.com>
2016-03-15 01:03:36 +0900

    1st Commit

---
+++ xcode/linkingSampler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  linkingSampler
+//
+//  Created by Satorupan on 2016/03/15.
+//  Copyright © 2016年 paper company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcode/linkingSampler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  linkingSampler
+//
+//  Created by Satorupan on 2016/03/15.
+//  Copyright © 2016年 paper company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
